9cb644b87589a3add1948f8b9e5518134f5c0046
Hamza Ghelle <hamzaghellegmail.com>
2021-10-16 14:22:48 -0400

    created tables for data base

---
+++ db/schema/04_quiz_restuls.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS quiz_results CASCADE;
+
+CREATE TABLE quiz_results (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  quiz_id INTEGER REFERENCES quizzes(id) ON DELETE CASCADE,
+  result SMALLINT
+);
--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,8 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email, password) VALUES ('Alice', 'AliceWonderland@fantasy.com', 'passowrd');
+INSERT INTO users (name, email, password) VALUES ('Kira', 'KiraKnight@Actors.com', 'password');
+
+INSERT INTO quizzes(user_id, title) VALUES (1, 'Basketball quiz');
+INSERT INTO quizzes(user_id, title) VALUES (2, 'Hockey quiz');
+
+INSERT INTO quiz_questions(quiz_id)
