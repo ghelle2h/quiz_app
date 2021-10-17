7baed187df65119c8d782ac4c7776ba770b0c858
Navya Rajput <navyasrajput@gmail.com>
2021-10-17 10:05:30 -0400

    Added isPrivate attribute to quizzes table as per requirement

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -2,7 +2,7 @@
 INSERT INTO users (name, email, password) VALUES ('Alice', 'AliceWonderland@fantasy.com', 'passowrd');
 INSERT INTO users (name, email, password) VALUES ('Kira', 'KiraKnight@Actors.com', 'password');
 
-INSERT INTO quizzes(user_id, title) VALUES (1, 'Basketball quiz');
-INSERT INTO quizzes(user_id, title) VALUES (2, 'Hockey quiz');
+INSERT INTO quizzes(user_id, title, isPrivate) VALUES (1, 'Basketball quiz', FALSE);
+INSERT INTO quizzes(user_id, title, isPrivate) VALUES (2, 'Hockey quiz',FALSE);
 
-INSERT INTO quiz_questions(quiz_id)
+INSERT INTO quiz_questions(quiz_id);
