6513f83345bbf163cbaed1c8fa3d9bc42bb64e88
Navya Rajput <navyasrajput@gmail.com>
2021-10-17 20:12:44 -0400

    Fixed quiz_questions in 01_user

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -5,6 +5,9 @@ INSERT INTO users (name, email, password) VALUES ('Kira', 'KiraKnight@Actors.com
 INSERT INTO quizzes(user_id, title, isPrivate) VALUES (1, 'Basketball quiz', FALSE);
 INSERT INTO quizzes(user_id, title, isPrivate) VALUES (2, 'Hockey quiz',FALSE);
 
-INSERT INTO quiz_questions(1, "How many points is a three pointer", 3, TRUE);
-INSERT INTO quiz_questions(1, "How many points is a layup", 2, TRUE);
-INSERT INTO quiz_questions(1, "How many points is a half-court shot", 3, TRUE);
+INSERT INTO quiz_questions(quiz_id, question, answer, correct)
+             VALUES (1, 'How many points is a three pointer', 3, TRUE);
+INSERT INTO quiz_questions(quiz_id, question, answer, correct)
+            VALUES (1, 'How many points is a layup', 2, TRUE);
+INSERT INTO quiz_questions(quiz_id, question, answer, correct)
+            VALUES (1, 'How many points is a half-court shot', 3, TRUE);
