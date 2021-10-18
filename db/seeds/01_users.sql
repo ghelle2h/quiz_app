9ede6229fd0ec0da407c76e36d2436d3fdf901cb
Navya Rajput <navyasrajput@gmail.com>
2021-10-17 21:44:50 -0400

    updated schema and seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -2,12 +2,28 @@
 INSERT INTO users (name, email, password) VALUES ('Alice', 'AliceWonderland@fantasy.com', 'passowrd');
 INSERT INTO users (name, email, password) VALUES ('Kira', 'KiraKnight@Actors.com', 'password');
 
+
 INSERT INTO quizzes(user_id, title, isPrivate) VALUES (1, 'Basketball quiz', FALSE);
 INSERT INTO quizzes(user_id, title, isPrivate) VALUES (2, 'Hockey quiz',FALSE);
 
-INSERT INTO quiz_questions(quiz_id, question, answer, correct)
-             VALUES (1, 'How many points is a three pointer', 3, TRUE);
-INSERT INTO quiz_questions(quiz_id, question, answer, correct)
-            VALUES (1, 'How many points is a layup', 2, TRUE);
-INSERT INTO quiz_questions(quiz_id, question, answer, correct)
-            VALUES (1, 'How many points is a half-court shot', 3, TRUE);
+INSERT INTO quiz_questions(quiz_id, question)
+             VALUES (1, 'How many points is a three pointer');
+INSERT INTO quiz_questions(quiz_id, question)
+            VALUES (1, 'How many points is a layup');
+INSERT INTO quiz_questions(quiz_id, question)
+            VALUES (1, 'How many points is a half-court shot');
+
+INSERT INTO quiz_answers(question_id, answer, isCorrect) VALUES (1, 3, TRUE);
+INSERT INTO quiz_answers(question_id, answer, isCorrect) VALUES (1, 2, FALSE);
+INSERT INTO quiz_answers(question_id, answer, isCorrect) VALUES (1, 1, FALSE);
+
+INSERT INTO quiz_answers(question_id, answer, isCorrect) VALUES (2, 2, TRUE);
+INSERT INTO quiz_answers(question_id, answer, isCorrect) VALUES (2, 1, FALSE);
+INSERT INTO quiz_answers(question_id, answer, isCorrect) VALUES (2, 3, FALSE);
+
+INSERT INTO quiz_answers(question_id, answer, isCorrect) VALUES (3, 3, TRUE);
+INSERT INTO quiz_answers(question_id, answer, isCorrect) VALUES (3, 1, FALSE);
+INSERT INTO quiz_answers(question_id, answer, isCorrect) VALUES (3, 4, FALSE);
+
+INSERT INTO answer_attempts(answer_id, user_id) VALUES (1, 1);
+INSERT INTO answer_attempts(answer_id, user_id) VALUES (2, 1);
