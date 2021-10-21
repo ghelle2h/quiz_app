46272cb2723ff900e3315597644d3e4cb9fc8b89
Navya Rajput <navyasrajput@gmail.com>
2021-10-21 13:35:56 -0400

    Updated the schema seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -5,6 +5,8 @@ INSERT INTO users (name, email, password) VALUES ('Kira', 'KiraKnight@Actors.com
 
 INSERT INTO quizzes(user_id, title, isPrivate) VALUES (1, 'Basketball quiz', FALSE);
 INSERT INTO quizzes(user_id, title, isPrivate) VALUES (2, 'Hockey quiz',FALSE);
+INSERT INTO quizzes(user_id, title, description, isPrivate) VALUES (2, 'National Animal quiz', 'Test your knowledge on National Animals', FALSE);
+
 
 INSERT INTO quiz_questions(quiz_id, question)
              VALUES (1, 'How many points is a three pointer');
@@ -12,6 +14,14 @@ INSERT INTO quiz_questions(quiz_id, question)
             VALUES (1, 'How many points is a layup');
 INSERT INTO quiz_questions(quiz_id, question)
             VALUES (1, 'How many points is a half-court shot');
+
+INSERT INTO quiz_questions(quiz_id, question)
+            VALUES (3, 'Which country''s national animal is a bald eagle');
+INSERT INTO quiz_questions(quiz_id, question)
+            VALUES (3, 'Which country''s national animal is a lion');
+INSERT INTO quiz_questions(quiz_id, question)
+            VALUES (3, 'Which country''s national animal is a tiger');
+
 
 INSERT INTO quiz_answers(question_id, answer, isCorrect) VALUES (1, 3, TRUE);
 INSERT INTO quiz_answers(question_id, answer, isCorrect) VALUES (1, 2, FALSE);
@@ -25,5 +35,25 @@ INSERT INTO quiz_answers(question_id, answer, isCorrect) VALUES (3, 3, TRUE);
 INSERT INTO quiz_answers(question_id, answer, isCorrect) VALUES (3, 1, FALSE);
 INSERT INTO quiz_answers(question_id, answer, isCorrect) VALUES (3, 4, FALSE);
 
+INSERT INTO quiz_answers(question_id, answer, isCorrect) VALUES (4, 1, TRUE);
+INSERT INTO quiz_answers(question_id, answer, isCorrect) VALUES (4, 2, FALSE);
+INSERT INTO quiz_answers(question_id, answer, isCorrect) VALUES (4, 3, FALSE);
+INSERT INTO quiz_answers(question_id, answer, isCorrect) VALUES (4, 4, FALSE);
+INSERT INTO quiz_answers(question_id, answer, isCorrect) VALUES (5, 1, TRUE);
+INSERT INTO quiz_answers(question_id, answer, isCorrect) VALUES (5, 2, FALSE);
+INSERT INTO quiz_answers(question_id, answer, isCorrect) VALUES (5, 3, FALSE);
+INSERT INTO quiz_answers(question_id, answer, isCorrect) VALUES (5, 4, FALSE);
+INSERT INTO quiz_answers(question_id, answer, isCorrect) VALUES (6, 1, FALSE);
+INSERT INTO quiz_answers(question_id, answer, isCorrect) VALUES (6, 2, TRUE);
+INSERT INTO quiz_answers(question_id, answer, isCorrect) VALUES (6, 3, FALSE);
+INSERT INTO quiz_answers(question_id, answer, isCorrect) VALUES (6, 4, FALSE);
+
+INSERT INTO quiz_attempts (quiz_id, user_id) VALUES (3, 2);
+
 INSERT INTO answer_attempts(answer_id, user_id) VALUES (1, 1);
 INSERT INTO answer_attempts(answer_id, user_id) VALUES (2, 1);
+
+INSERT INTO answer_attempts(answer_id, user_id, quiz_attempt_id, question_id) VALUES (10, 2, 1, 4); --TRUE
+INSERT INTO answer_attempts(answer_id, user_id, quiz_attempt_id, question_id) VALUES (15, 2, 1, 5); --FALSE
+INSERT INTO answer_attempts(answer_id, user_id, quiz_attempt_id, question_id) VALUES (19, 2, 1, 6); --TRUE
+
